35e42bf22769ab505ce3530ad0dbe52345c2cd74
arjbholu@gmail.com <arjbholu@gmail.com>
2017-12-11 18:59:51 +0530

    Delete Files

--- README.md
+++
@@ -1,7 +0,0 @@
-# BMICalculator
-
-This is a simple app developed in React for Calulating Body Mass Index!
-
-I have used webpack for configuration.
-
-To access the app : []
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import App from './App/app.js'
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
